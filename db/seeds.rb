3bed4d40a4d0f9ac103657deb68baa843cb6f197
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-13 21:32:44 -0700

    shelters added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+franks = Shelter.create!(foster_program: true,
+          name: "frankie's friendly felines",
+          city: "westminster", rank: 10)
+
+petes = Shelter.create!(foster_program: true,
+          name: "Pete's Parrakeets", city: "Newminster", rank: 9)
+
+needs = Shelter.create!(foster_program: true,
+          name: "Need a pom", city: "oldminster", rank: 8)
