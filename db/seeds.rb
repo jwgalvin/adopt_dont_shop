aa5bb25d338e9cadb8cf7f4d884a39dcab1f0805
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-14 09:59:46 -0700

    Destroyall's added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+
+PetApplication.destroy_all
+Application.destroy_all
+Pet.destroy_all
+Shelter.destroy_all
+VeterinaryOffice.destroy_all
+Veterinarian.destroy_all# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
